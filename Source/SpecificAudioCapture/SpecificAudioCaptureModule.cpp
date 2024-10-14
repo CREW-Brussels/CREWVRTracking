1b22c3c355f10c63a79646244b29ad5ea41c9252
vivian-crew <vivian@crew.brussels>
2024-10-14 17:45:27 +0200

    Moved Specific Audio Capture into its own plugin

--- Source/SpecificAudioCapture/SpecificAudioCaptureModule.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright HTC Corporation. All Rights Reserved.
-
-#include "SpecificAudioCaptureModule.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FSpecificAudioCaptureModule, SpecificAudioCapture)
